1d6b57828fcb48b34c754faf54df4f90bc546c1f
aj92singh <ajay_92singh@yahoo.co.in>
2018-05-24 09:55:44 +0530

    Add files via upload

---
+++ DemoWebAPI/Startup.cs
@@ -0,0 +1,50 @@
+﻿using DemoWebAPI.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DemoWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            var connection = @"Data Source=sampleserver-aj.database.windows.net;Initial Catalog=SampleDatabase;User ID=testuser;Password=Azure@1234";
+            services.AddDbContext<SampledatabaseContext>(options => options.UseSqlServer(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Employees/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Employees}/{action=GetEmployee}/{id?}");
+            });
+        }
+    }
+}
